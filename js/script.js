42ce38c6f85d6d76f1beb1d5408521a52033a3be
dik4is <dik4iskandar@gmail.com>
2025-07-27 20:10:43 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+const nav = document.getElementById('nav');
+
+window.addEventListener('scroll', function(){
+    scrollposition = window.scrollY;
+    
+    if (scrollposition >=30){
+        nav.classlist.add('nav-dark');
+    } else if (scrollposition <=30){
+        nav.classlist.remove('nav-dark');
+    }
+})
 
